b622f13061f3654a0ee0d4418a27146f2940a2ca
David Caffrey <dave.caffrey71@gmail.com>
2023-08-21 21:08:43 +0100

    faq dropdown working

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-console.log("hello world!");
 let question = document.getElementsByClassName('question');
+
+let answers = document.getElementsByClassName('answer');
 
 for (let i = 0; i < question.length; i++) {
     question[i].addEventListener('click', function() {
         this.classList.toggle('active');
+        this.chi
         let answer = this.nextElementSibling;
         if (answer.style.display === 'block') {
             answer.style.display = 'none';
         } else {
+            for (let i = 0; i < answers.length; i++) {
+                answers[i].style.display = 'none';
+            }
             answer.style.display = 'block';
+            answer.style.margin = '0 0 0 0';
         }
     });
 }
