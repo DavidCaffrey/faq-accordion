14be615a8077ff62e8f77abc90fb2a4deba8bbe8
David Caffrey <dave.caffrey71@gmail.com>
2023-08-21 06:59:22 +0100

    worked on accordion action

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("hello world!");
+let question = document.getElementsByClassName('question');
+
+for (let i = 0; i < question.length; i++) {
+    question[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        let answer = this.nextElementSibling;
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+}
