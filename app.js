d5e1fb686addd0c750c9b6d712403f81ec2530b8
David Caffrey <dave.caffrey71@gmail.com>
2023-08-22 21:23:15 +0100

    mobile complete

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ let question = document.getElementsByClassName('question');
 let answers = document.getElementsByClassName('answer');
 
 const arrowRotate = [
-    // { transform: 'rotate(0)' },
-    // { transform: 'rotate(-90deg)' },
-    { transform: 'rotate(-180deg)' }
+    { transform: 'rotate(0)' },
+    { transform: 'rotate(-180deg)' },
+    { transform: 'rotate(0)' },
 ];
 
 const arrowTimings = {
-    duration: 1000,
-    iterations: 1,
+    duration: 2000,
+    iterations: 1
 }
 
 
@@ -19,7 +19,6 @@ for (let i = 0; i < question.length; i++) {
 
     question[i].addEventListener('click', function() {
         this.classList.toggle('active');
-        console.log(this.childNodes[1].childNodes[0]);
         this.childNodes[1].childNodes[0].animate(arrowRotate, arrowTimings);
         let answer = this.nextElementSibling;
         if (answer.style.display === 'block') {
