edc14166a4f22e83676fa9a736434b60fc2882a4
David Caffrey <dave.caffrey71@gmail.com>
2023-08-22 06:43:20 +0100

    animated arrow using js

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ let question = document.getElementsByClassName('question');
 
 let answers = document.getElementsByClassName('answer');
 
+const arrowRotate = [
+    // { transform: 'rotate(0)' },
+    // { transform: 'rotate(-90deg)' },
+    { transform: 'rotate(-180deg)' }
+];
+
+const arrowTimings = {
+    duration: 1000,
+    iterations: 1,
+}
+
+
+
 for (let i = 0; i < question.length; i++) {
+
     question[i].addEventListener('click', function() {
         this.classList.toggle('active');
-        this.chi
+        console.log(this.childNodes[1].childNodes[0]);
+        this.childNodes[1].childNodes[0].animate(arrowRotate, arrowTimings);
         let answer = this.nextElementSibling;
         if (answer.style.display === 'block') {
             answer.style.display = 'none';
